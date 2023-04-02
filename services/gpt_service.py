22a75288ae7f0e5f2893e7dcc087d751653f7db5
Sulthan Afif Althaf <sulthan.afif@ui.ac.id>
2023-04-02 22:30:05 +0700

    [Feat] audio and edit API
    - Implement audio API tto translate/transcript audio file
    - add requirements
    - add service to support API

--- services/gpt_service.py
+++ services/gpt_service.py
@@ -3,7 +3,10 @@ import openai
 from dotenv import load_dotenv
 from models.chat_model import Chat
 from models.image_model import Image
+from models.edit_model import Edit
 load_dotenv()
+
+import time
 
 openai.api_key = str(os.getenv('OPENAI_API_KEY'))
 openai.Model.list()
@@ -12,7 +15,7 @@ class GPTService :
     def __init__(self) -> None:
         self.urls = 'https://api.openai.com/v1/'
 
-    def chat_completion(self, prompt):
+    async def chat_completion(self, prompt):
         response = openai.ChatCompletion.create(
             model="gpt-3.5-turbo",
             messages=[
@@ -23,7 +26,7 @@ class GPTService :
         response_obj = Chat(**response)
         return response_obj
     
-    def image_generation(self, image_prompt):
+    async def image_generation(self, image_prompt):
         response = openai.Image.create(
             prompt= image_prompt,
             n=1,
@@ -31,3 +34,20 @@ class GPTService :
         )
         response_obj = Image(**response)
         return response_obj
+    
+    async def edit_text(self, input, instruction):
+        response = openai.Edit.create(
+            model="text-davinci-edit-001",
+            input=input,
+            instruction=instruction
+        )
+        response_obj = Edit(**response)
+        return response_obj
+    
+    async def audio_transcription(self, file):
+        response = openai.Audio.transcribe("whisper-1", file)
+        return response
+    
+    async def audio_translation(self, file):
+        response = openai.Audio.translate("whisper-1", file)
+        return response
